194cbb2859f9b5d894e181231a7dc88a5160e5b9
Darren Leith <leithdm@outlook.com>
2020-09-03 12:00:53 +0100

    exercise 1

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>My first React-Native app</Text>;
+
+    const greeting = 'Bye there!';
+    const anotherGreeting = 'My name is yoyo'
+    // const jsxElement = <Text>Hello to you!</Text>
+
+    return (
+    <View>
+        <Text style={styles.textStyle}>Getting started with react native!
+        </Text>
+        
+        <Text style={fontStyle.textStyle} >{anotherGreeting}
+        </Text>
+    </View>);
 };
 
-const styles = StyleSheet.create ( {
+const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
     }
 });
+
+const fontStyle = StyleSheet.create({
+    textStyle: {
+        fontSize: 20
+    }
+})
 
 export default ComponentsScreen; 
